e5da7946616d07fc4e36544d93bd9be5773f01be
ghoshgamer475 <ghoshgamer475@gmail.com>
2025-03-06 15:05:08 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,8 @@
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNumber = getRandomArbitrary(0, 100);
+console.log(randomNumber);
