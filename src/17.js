ffcb209eb411a737ed2f658b1ddecee78c49f31a
ghoshgamer475 <ghoshgamer475@gmail.com>
2025-03-22 15:58:04 +0000

    Add 17.js

---
+++ src/17.js
@@ -0,0 +1,7 @@
+function solveMathProblem(problem) {
+    // Add your solution logic here
+    return "The solution to " + problem + " is: [insert answer here]";
+}
+
+// Example usage:
+console.log(solveMathProblem("What is 5 + 7?"));
